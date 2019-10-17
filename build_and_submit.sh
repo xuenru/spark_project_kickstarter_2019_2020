96368b02d557e1d88a5283b9b5f4f3e7a68f546e
XU Li <xuenru@gmail.com>
2019-10-17 08:51:14 +0200

    change config of path_to_spark

--- build_and_submit.sh
+++ build_and_submit.sh
@@ -17,7 +17,7 @@ sbt assembly || { echo 'Build failed' ; exit 1; }
 
 echo -e "\n --- spark-submit --- \n"
 
-path_to_spark="$HOME/spark-2.3.4-bin-hadoop2.7"
+path_to_spark="$HOME/data/enst/opt/spark"
 
 if [ -n "$2" ]; then path_to_spark=$2; fi
 
