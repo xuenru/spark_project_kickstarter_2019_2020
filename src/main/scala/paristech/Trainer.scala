ebce7c308c9724221559dc5bfd03b36f514f8d10
XU Li <xuenru@gmail.com>
2019-10-26 21:26:03 +0200

    tp3 part 1

--- src/main/scala/paristech/Trainer.scala
+++ src/main/scala/paristech/Trainer.scala
@@ -1,6 +1,8 @@
 package paristech
 
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel, IDF, OneHotEncoderEstimator, RegexTokenizer, StopWordsRemover, StringIndexer, VectorAssembler}
 import org.apache.spark.sql.SparkSession
 
 
@@ -27,20 +29,101 @@ object Trainer {
       .getOrCreate()
 
 
-    /*******************************************************************************
+    /** *****************************************************************************
       *
-      *       TP 3
+      * TP 3
       *
       *       - lire le fichier sauvegarder précédemment
       *       - construire les Stages du pipeline, puis les assembler
       *       - trouver les meilleurs hyperparamètres pour l'entraînement du pipeline avec une grid-search
       *       - Sauvegarder le pipeline entraîné
       *
-      *       if problems with unimported modules => sbt plugins update
+      * if problems with unimported modules => sbt plugins update
       *
-      ********************************************************************************/
+      * *******************************************************************************/
 
     println("hello world ! from Trainer")
 
+    // Charger le DataFrame obtenu à la fin du TP 2.
+    val data = spark.read.parquet("data/tp2_df_final.parquet")
+    data.show()
+
+    // Stage 1 : récupérer les mots des textes
+    val tokenizer = new RegexTokenizer()
+      .setPattern("\\W+")
+      .setGaps(true)
+      .setInputCol("text")
+      .setOutputCol("tokens")
+    val dataTokens = tokenizer.transform(data)
+    dataTokens.select("tokens").show()
+
+    // Stage 2 : retirer les stop words
+    val stopWordsRemover = new StopWordsRemover()
+      .setInputCol("tokens")
+      .setOutputCol("words")
+
+    val dataWords = stopWordsRemover.transform(dataTokens)
+    dataWords.select("words").show(false)
+
+    // Stage 3 : computer la partie TF
+    val cvModel: CountVectorizerModel = new CountVectorizer()
+      .setInputCol("words")
+      .setOutputCol("TF")
+      .setVocabSize(3)
+      .setMinDF(2)
+      .fit(dataWords)
+
+    val dataTF = cvModel.transform(dataWords)
+    dataTF.select("TF").show(false)
+
+    val idf = new IDF()
+      .setInputCol("TF")
+      .setOutputCol("tfidf")
+
+    val idfModel = idf.fit(dataTF)
+
+    val dataTFIDF = idfModel.transform(dataTF)
+    dataTFIDF.select("tfidf").show(false)
+
+    val countryIndexer = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+
+    val DataCountryIndexed = countryIndexer.fit(dataTFIDF).transform(dataTFIDF)
+    DataCountryIndexed.show()
+
+    val currencyIndexer = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+
+    val DataCCIndexed = currencyIndexer.fit(DataCountryIndexed).transform(DataCountryIndexed)
+    DataCCIndexed.select("country2", "currency2", "country_indexed", "currency_indexed").show()
+
+    val encoder = new OneHotEncoderEstimator()
+      .setInputCols(Array("country_indexed", "currency_indexed"))
+      .setOutputCols(Array("country_onehot", "currency_onehot"))
+    val dataOneHot = encoder.fit(DataCCIndexed).transform(DataCCIndexed)
+    dataOneHot.select("country_indexed", "currency_indexed", "country_onehot", "currency_onehot").show()
+
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "country_onehot", "currency_onehot"))
+      .setOutputCol("features")
+
+    val dataAssembled = assembler.transform(dataOneHot)
+    dataAssembled.select("tfidf", "days_campaign", "hours_prepa", "goal", "country_onehot", "currency_onehot", "features").show()
+    dataAssembled.select("features").show(false)
+
+    val lr = new LogisticRegression()
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_prediction")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(20)
   }
 }
